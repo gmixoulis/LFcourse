04cf81bff3769de61acfed93713258a39773b2ca
George Michoulis <gmichoulis@sidroco.com>
2025-11-19 02:37:36 +0200

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: " ",
+        ghost: "hover:text-accent-foreground ",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
